53c30fbc49412e864aba157c5f13dcf773f350bc
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-17 05:56:22 +0100

    added shader, vertex shader source code

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,21 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -15,6 +30,12 @@ int main() {
         return -1;
     }
     glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 800);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
 
     while (!glfwWindowShouldClose(window)) {
 
