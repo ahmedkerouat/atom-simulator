279997e2195502d66eab48002a0a61eb2a99218a
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-19 05:32:01 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,50 +1,19 @@
-#include <iostream>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
-// Vertex Shader source code
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-//Fragment Shader source code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
+#define GLFW_INCLUDE_NONE
+#include <glad/glad.h>
+#include <iostream>
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Atom Simulator", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create the window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
+	glfwInit();
 
-    glViewport(0, 0, 800, 800);
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Window creation failed";
+	}
 
-    while (!glfwWindowShouldClose(window)) {
-
-        glfwPollEvents();
-
-    }
-
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
