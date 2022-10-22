53403b32e375b5073907e3fb54059c05dea68c4f
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-22 15:49:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from vpython import *
-
+from atom import Atom
 
 time = 0  # Time in simulation.
 dt = 0.01  # Time step size.
 
-
-electron = sphere(radius=0.1, color=vector(
-    1, 1, 0), make_trail=True, retain=300, trail_color=vector(1, 1, 1), opacity=1.0)
-
-proton = sphere(radius=0.35, color=vector(
-    1, 0, 0), opacity=1.0)
-
+hydrogen = Atom(1, 1)
+hydrogen.build()
 while (time <= 1000):
     rate(100)  # Number of frames/loops per second.
-    electron.pos.x = cos(time)
-    electron.pos.y = sin(time)
+
+    hydrogen.animate(time)
+
     time = time + dt
