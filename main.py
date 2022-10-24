5eda3b9cb803147fad9bf5bb7c111a9dc030d651
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-24 06:02:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from atom import Atom
 time = 0  # Time in simulation.
 dt = 0.01  # Time step size.
 
-hydrogen = Atom(1, 1)
-hydrogen.build()
+a = 1
+z = 4
+
+atom = Atom(a, z)
+atom.get_points()
+atom.build()
+
 while (time <= 1000):
     rate(100)  # Number of frames/loops per second.
 
-    hydrogen.animate(time)
-
+    atom.animate(time)
     time = time + dt
