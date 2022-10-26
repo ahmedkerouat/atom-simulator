c829db66bfcb0ceb20d9e5ab8bbf7618221dedc1
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-26 20:23:36 +0100

    Update atom.py

--- atom.py
+++ atom.py
@@ -4,10 +4,10 @@ from random import random, randint
 
 class Atom():
 
-    def __init__(self, a_atom, z_atom):
+    def __init__(self, atomic_num, num_charges):
 
-        self.a_atom = a_atom
-        self.z_atom = z_atom
+        self.atomic_num = atomic_num
+        self.num_charges = num_charges
 
         self.nucleons = []
         self.electrons = []
@@ -46,18 +46,35 @@ class Atom():
                 self.best_points = points[:]
             points[closest_index] = (x, y, z)
 
+    def get_electron_configuration(self):
+        orbitals = "1s 2s 2p 3s 3p 4s 3d 4p 5s 4d 5p 6s 4f 5d 6p 7s 5f 6d 7p 6f 7d 7f".split()
+        possible_electrons = dict(s=2, p=6, d=10, f=14)
+        electron_count = 0
+        result = []
+        for i in orbitals:
+            if electron_count < self.atomic_num:
+                orbital = "".join(j for j in i if j in 'spdf')
+                result.append(i+str(possible_electrons[orbital]))
+                electron_count += possible_electrons[orbital]
+            else:
+                break
+        if electron_count > self.atomic_num:
+            dif = electron_count - self.atomic_num
+            last_electron_pos = result[-1].find(orbital)+1
+            last_electrons = int(result[-1][last_electron_pos:])
+            new_num = last_electrons - dif
+            result[-1] = result[-1][:last_electron_pos] + str(new_num)
+        self.config_result = (' '.join(result))
+
     def build(self):
-        for _, coords in zip(range(1, self.z_atom + 1), self.best_points):
+        for _, coords in zip(range(1, self.num_charges + 1), self.best_points):
             proton = sphere(radius=0.35, color=vector(
                 1, 0, 0), opacity=1.0, pos=vector(*coords))
 
-        for electron in range(1, self.z_atom + 1):
+        for electron in range(1, self.num_charges + 1):
             electron = sphere(radius=0.1, color=vector(
                 1, 1, 0), make_trail=True, retain=300, trail_color=vector(1, 1, 1), opacity=1.0, pos=vector(0, 0, 0))
             self.electrons.append(electron)
-
-    def get_electron_configuration(self):
-        pass
 
     def animate(self, time):
         pass
