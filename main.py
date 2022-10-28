8838d584646e500d37dcaede3ae7b2f0e3014e3b
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-10-28 13:04:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from atom import Atom
 time = 0  # Time in simulation.
 dt = 0.01  # Time step size.
 
-A = 1
-Z = 4
+A = 1  # Number of nucleons
+Z = 4  # Number of charges / Atomic Number
 
 atom = Atom(A, Z)
 atom.get_points()
@@ -14,5 +14,5 @@ atom.build()
 
 while (time <= 1000):
     rate(100)  # Number of frames/loops per second.
-
+    # atom.animate(time)
     time = time + dt
