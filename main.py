485c68b8c5c58780585afa7cc7ed2a0702aeeeb0
Ahmed <cmoiminp@gmail.com>
2022-11-19 13:17:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ run = True
 dt = 100  # Time step size.
 time = 0
 
-A = 6  # Number of nucleons
-Z = 3  # Number of charges / Atomic Number
+A = 8  # Number of nucleons
+Z = 4  # Number of charges / Atomic Number
+
+names = []
+symbols = []
+numbers_of_charges = []
+
+file = open("nuclides.txt")
+informations = file.readlines()
+
+for _ in informations:
+    information = _.split()
+    names.append(information[0])
+    symbols.append(information[1]) 
+    numbers_of_charges.append(information[2]) 
+
+print(names, symbols, numbers_of_charges)
 
 
 if 0 < Z <= 118 and type(A) == int and type(Z) == int:
 
     atom = Atom(A, Z)
-    wtext(text="\nNeutrons : " + str(A - Z) + "\nProtons : " + str(Z) + "\nElectrons : " + str(Z))
+    wtext(text="\nName : "+ atom.name  + "\nNeutrons : " + str(A - Z) + "\nProtons : " + str(Z) + "\nElectrons : " + str(Z))
+    menu(choices=names, bind=atom.name, selected=atom.name)
     atom.get_electron_configuration()
     if A >= 4:
         atom.get_points()
